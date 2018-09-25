b70e470aad03e8fcd3dfa3e0991ba01555c07cc4
Tonmoy Sardar <tonmoy.1984@gmail.com>
2018-09-25 11:48:05 +0530

    update

--- app/core/globals.ts
+++ app/core/globals.ts
@@ -3,5 +3,5 @@ export const apiEndpoint: string = 'http://132.148.147.239:8001/';
 export const img_base_url: string = 'http://132.148.147.239:8001';
 export const itemPerPage: number = 10;
 export const paginationMaxSize: number = 2;
-export const server_key: string = 'AIzaSyBUhrHXH7JH8hK2RyuWeqDogtwr2UdsUAE';
+export const server_key: string = 'AIzaSyBPdXWCWf5uOL28k4hWy1njSWM22w5AAFk';
 export const firebase_url: string = 'https://fcm.googleapis.com/fcm/send';
